7769f9714700beca92165fe55e99858abb1f60bd
Jason Christensen <JasonC@ip-corporation.com>
2026-05-20 14:56:16 -0500

    Supabase fix

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -2,7 +2,7 @@ import { createClient as createBrowserClient } from '@supabase/supabase-js'
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-type CookieStore = ReturnType<typeof cookies>
+type CookieStore = Awaited<ReturnType<typeof cookies>>
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
