2646f9bd17f2a0ca0e0501f5a94c1ba896849969
Jason Christensen <JasonC@ip-corporation.com>
2026-05-20 14:30:07 -0500

    Update

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient as createBrowserClient } from '@supabase/supabase-js'
 import { createServerClient } from '@supabase/ssr'
-import type { Cookies } from 'next/headers'
+import type { cookies, Cookies } from 'next/headers'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
